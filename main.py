af698ecc83663626428377e2ef876570f5d97050
Dasha-MyOleg <dasharikong@gmail.com>
2023-07-01 04:36:39 +0300

    dirty_2
    better code
    better background and restart screen

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 
 import pygame
 
-
 clock = pygame.time.Clock()
+game_speed = 9
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-#screen = pygame.display.set_mode((600, 300). flags=pygame.NOFRAME) - без рамки
+
+screen_width_start = 0
+screen_heigh_start = 0
+screen_width = 1280
+screen_heigh = 720
+screen = pygame.display.set_mode((screen_width, screen_heigh))
+#screen = pygame.display.set_mode((screen_width, screen_heigh). flags=pygame.NOFRAME) - без рамки
+
 pygame.display.set_caption("Dirty Rikong Game")
 icon = pygame.image.load('images/icon.png').convert_alpha()
 pygame.display.set_icon(icon)
@@ -14,19 +21,17 @@ pygame.display.set_icon(icon)
 #square = pygame.Surface((75,55))
 #square.fill('Blue')
 
-
 #myfont = pygame.font.Font('FontsText/SpaceMono-Regular.ttf',40)
 #text_surfase = myfont.render('Rikong love you',False,'Red','Blue')
 
-bg = pygame.image.load('images/bg.png').convert()
+
 player = pygame.image.load('images/player_right/right_1.png')
-
-rat = pygame.image.load('images/rat.png').convert_alpha()
-#rat_x = 1290
-rat_list_in_game =[]
-
 player_anim_count = 0
-bg_x=0
+player_speed = 15
+player_x = 10
+player_y = 420
+player_x_min = 0
+player_x_max = 600
 
 walk_left = [
     pygame.image.load('images/player_left/left_1.png').convert_alpha(),
@@ -42,38 +47,77 @@ walk_right = [
     pygame.image.load('images/player_right/right_4.png').convert_alpha(),
 ]
 
-player_speed = 10
-player_x = 10
-player_y = 380
-
 is_jump = False
-jump_count = 15
+jump_count_start = 15
+jump_count = jump_count_start
+
+
+
+bg_width_start = 0
+bg_width_end = 2500
+
+bg_sky = pygame.image.load('images/bg/bg_sky.png').convert_alpha()
+bg_sky_x=0
+bg_mountain_back = pygame.image.load('images/bg/bg_mountain_back.png').convert_alpha()
+bg_mountain_back_x=0
+bg_mountain_front = pygame.image.load('images/bg/bg_mountain_front.png').convert_alpha()
+bg_mountain_front_x=0
+bg_grass = pygame.image.load('images/bg/bg_grass.png').convert_alpha()
+bg_grass_x=0
+
+bg_sky_speed = 2
+bg_mountain_back_speed = 4
+bg_mountain_front_speed = 10
+bg_grass_speed = 20
+
 
 bg_sound = pygame.mixer.Sound('sounds/soundtrack.mp3')
 bg_sound.play()
 
+
+
 rat_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(rat_timer,10000)
+rat_per_millisecond = 10000
+pygame.time.set_timer(rat_timer,rat_per_millisecond)
+rat = pygame.image.load('images/rat.png').convert_alpha()
+#rat x and y = 1290,450
+rat_list_in_game =[]
+rat_speed = 20
+
 
 blasters_left = 5
 blast = pygame.image.load('images/blast.png').convert_alpha()
 blasts = []
+blast_speed = 11
+
 
 gameplay = True
-label = pygame.font.Font('FontsText/SpaceMono-Regular.ttf',70)
-label_2 = pygame.font.Font('FontsText/Foldit-VariableFont_wght.ttf', 60)
-lose_label = label.render('You lose!',False,(0,0,0))
-restart_label = label.render('Restart?',False,(0,0,0))
-restart_label_rect = restart_label.get_rect(topleft=(420, 300))
-blast_label = label_2.render('Blasts left:',True,(230, 240, 255))
+
+label_text_size = 200
+label = pygame.font.Font('FontsText/VT323-Regular.ttf',label_text_size)
+lose_label = label.render('You lose!',False,"Black")
+restart_label = label.render('Restart',False,"Black")
+restart_screen = pygame.image.load('images/lose_screen.png')
+
+lose_label_location = (30,40)
+restart_label_location = (30,200)
+restart_label_rect = restart_label.get_rect(topleft=(restart_label_location))
+
+
+
 
 running = True
 while running:
 
-    #персонаж
-    screen.blit(bg, (bg_x, 0))
-    screen.blit(bg, (bg_x+1280, 0))
-    #screen.blit(rat, (rat_x, 450))
+    #рух фону
+    screen.blit(bg_sky, (bg_sky_x, bg_width_start))
+    screen.blit(bg_sky, (bg_sky_x + bg_width_end, bg_width_start))
+    screen.blit(bg_mountain_back, (bg_mountain_back_x, bg_width_start))
+    screen.blit(bg_mountain_back, (bg_mountain_back_x + bg_width_end, bg_width_start))
+    screen.blit(bg_mountain_front, (bg_mountain_front_x, bg_width_start))
+    screen.blit(bg_mountain_front, (bg_mountain_front_x + bg_width_end, bg_width_start))
+    screen.blit(bg_grass, (bg_grass_x, bg_width_start))
+    screen.blit(bg_grass, (bg_grass_x + bg_width_end, bg_width_start))
 
     if gameplay:
 
@@ -81,9 +125,9 @@ while running:
         if rat_list_in_game:
             for (i, el) in enumerate (rat_list_in_game):
                 screen.blit(rat, el)
-                el.x -= 16
+                el.x -= rat_speed
 
-                if el.x < -200:
+                if el.x < screen_width_start - 200:
                     rat_list_in_game.pop()
 
                 if player_hitbox.colliderect(el):
@@ -96,9 +140,9 @@ while running:
             screen.blit(walk_right[player_anim_count], (player_x, player_y))
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and player_x > 0:  # left
+        if keys[pygame.K_a] and player_x > player_x_min:
             player_x -= player_speed
-        elif keys[pygame.K_d] and player_x < 400:  # right
+        elif keys[pygame.K_d] and player_x < player_x_max:
             player_x += player_speed
 
         if not is_jump:
@@ -106,7 +150,7 @@ while running:
                 is_jump = True
 
         else:
-            if jump_count >= -15:
+            if jump_count >= -jump_count_start:
                 if jump_count > 0:
                     player_y -= (jump_count ** 2) / 3
                 else:
@@ -114,16 +158,29 @@ while running:
                 jump_count -= 1
             else:
                 is_jump = False
-                jump_count = 15
+                jump_count = jump_high = jump_count_start
 
         if player_anim_count == 3:
             player_anim_count = 0
         else:
             player_anim_count += 1
 
-        bg_x -= 5
-        if bg_x == -1280:
-            bg_x = 0
+        bg_sky_x -= bg_sky_speed
+        if bg_sky_x == -bg_width_end:
+           bg_sky_x = bg_width_start
+
+        bg_mountain_back_x -= bg_mountain_back_speed
+        if bg_mountain_back_x == -bg_width_end:
+           bg_mountain_back_x = bg_width_start
+
+        bg_mountain_front_x -= bg_mountain_front_speed
+        if bg_mountain_front_x == -bg_width_end:
+            bg_mountain_front_x = bg_width_start
+
+        bg_grass_x -= bg_grass_speed
+        if bg_grass_x == -bg_width_end:
+           bg_grass_x = bg_width_start
+
 
         #screen.blit(blast_label, "blasters_left", (400, 100))
 
@@ -135,9 +192,9 @@ while running:
         if blasts:
             for el in blasts:
                 screen.blit(blast,(el.x,el.y))
-                el.x += 7
+                el.x += blast_speed
 
-                if el.x > 1360:
+                if el.x > screen_width - 80:
                     blasts.pop(i)
                     blasters_left += 1
 
@@ -153,10 +210,12 @@ while running:
 
         #screen.blit(square, (230, 380))
 
-        clock.tick(9)
+        clock.tick(game_speed)
+
     else:
-        screen.fill((87,88,89))
-        screen.blit(lose_label,(400,100))
+
+        screen.blit(restart_screen, (screen_width_start,screen_heigh_start))
+        screen.blit(lose_label,lose_label_location)
         screen.blit(restart_label,restart_label_rect)
 
         mouse = pygame.mouse.get_pos()
@@ -175,11 +234,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
         if event.type == rat_timer:
-           rat_list_in_game.append(rat.get_rect(topleft=(1270,450)))
+           rat_list_in_game.append(rat.get_rect(topleft=(screen_width - 15,screen_heigh - 270)))
+
         if gameplay and event.type == pygame.KEYUP and event.key == pygame.K_f and blasters_left > 0:
-            blasts.append(blast.get_rect(topleft=(player_x+100,player_y+110)))
+            blasts.append(blast.get_rect(topleft=(player_x + 100, player_y + 127)))
             blasters_left -= 1
-
-
-
